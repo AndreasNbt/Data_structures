078f86a811d6517b90447377c31865b42a9fd7a1
Andreas <andreasn@csd.auth.gr>
2021-05-08 16:48:14 +0300

    Comments

--- Data_structures/array.cpp
+++ Data_structures/array.cpp
@@ -1,7 +1,8 @@
 #include "array.h"
-
 #include <iostream>
 
+
+//array constructor
 array::array(int size)
 {
     words = new std::string[size];
@@ -9,6 +10,7 @@ array::array(int size)
     length = 0;
 }
 
+//ordering search for the array
 int array::search(const std::string &word) const
 {
     for (int i=0;i<length;i++)
@@ -20,6 +22,7 @@ int array::search(const std::string &word) const
 
 }
 
+//using the search function, if the word is not in the array we add it, if the word is already in the array we just increase the counter of this word
 void array::insert(const std::string &word)
 {
     int index = search(word);
@@ -36,6 +39,7 @@ void array::insert(const std::string &word)
 
 }
 
+//using the search function, if we find the given word we delete it from the array
 void array::remove(const std::string &word)
 {
     int index = search(word);
@@ -49,6 +53,7 @@ void array::remove(const std::string &word)
     }
 }
 
+//a function to print the words and how many times they are found in the text
 void array::display() {
     for (int i=0;i<length;i++)
         std::cout << words[i] << ": " << wordCounter[i] << std::endl;
--- Data_structures/sorted_array.cpp
+++ Data_structures/sorted_array.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 #include "sorted_array.h"
 
+//sorted array constructor.
 sorted_array::sorted_array(int size) {
     words = new std::string[size];
     wordCounter = new int[size];
     length = 0;
 }
 
+//search function, we use the binary search method to find the given word.
 int sorted_array::search(const std::string &word) {
 
     int start = 0;
@@ -29,14 +31,17 @@ int sorted_array::search(const std::string &word) {
 
 }
 
+//operator overload for the brackets operator
 std::string &sorted_array::operator[](int index) {
     return words[index];
 }
 
+//a function to print the words and how many times they are found in the text
 void sorted_array::display() {
     for (int i=0;i<length;i++)
         std::cout << words[i] << ": " << wordCounter[i] << std::endl;
 }
+
 
 void sorted_array::insert(const std::string &word) {
     int index = search(word);
