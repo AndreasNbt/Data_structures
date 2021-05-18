87c9280b176c668aa5dfe36c074e5bc3198ee704
Andreas <andreasn@csd.auth.gr>
2021-05-18 20:44:39 +0300

    Node for hashtable done

--- Data_structures/HashTable.h
+++ Data_structures/HashTable.h
@@ -1,6 +1,16 @@
 #ifndef DATA_STRUCTURES_HASHTABLE_H
 #define DATA_STRUCTURES_HASHTABLE_H
 
+#include <string>
+#include <utility>
+
+struct HashNode {
+    std::string word;
+    int count;
+    HashNode *next;
+    HashNode() = delete;
+    explicit HashNode(std::string s) : word(std::move(s)), count(1), next(nullptr) {}
+};
 
 class HashTable {
 
