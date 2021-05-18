d7c35a0ccd3ceab8db62c300a6389031833413cd
Andreas <andreasn@csd.auth.gr>
2021-05-19 00:15:12 +0300

    Started working on the hashtable class

--- Data_structures/HashTable.cpp
+++ Data_structures/HashTable.cpp
@@ -1,1 +1,22 @@
 #include "HashTable.h"
+
+HashTable::HashTable() {
+    hashTable = new HashNode[Size];
+}
+
+
+int HashTable::HashFunc(const std::string &word) {
+    return 0;
+}
+
+
+void HashTable::Insert(const std::string &word) {
+
+
+
+}
+
+void HashTable::Search(const std::string &word) {
+
+}
+
--- Data_structures/HashTable.h
+++ Data_structures/HashTable.h
@@ -1,19 +1,29 @@
 #ifndef DATA_STRUCTURES_HASHTABLE_H
 #define DATA_STRUCTURES_HASHTABLE_H
+#define Size 99
 
 #include <string>
 #include <utility>
+#include <array>
 
 struct HashNode {
     std::string word;
     int count;
     HashNode *next;
-    HashNode() = delete;
+    HashNode() : count(0), next(nullptr) {}
     explicit HashNode(std::string s) : word(std::move(s)), count(1), next(nullptr) {}
 };
 
 class HashTable {
 
+private:
+    HashNode *hashTable;
+    int HashFunc(const std::string &word);
+
+public:
+    HashTable();
+    void Insert(const std::string &word);
+    void Search(const std::string &word);
 };
 
 
