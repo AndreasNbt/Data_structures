1ae2e85f64245a6347b303b101283de5d36de930
Σταύρος <stavniko@csd.auth.gr>
2021-05-26 17:03:42 +0300

    New Erase

--- main.cpp
+++ main.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <fstream>
-#include <algorithm> // well...
 #include <sstream>
 #include <chrono>
 #include <random>
@@ -16,15 +15,22 @@
 //this is the method we use to lowercase every character in the text and remove the special characters from it to make our word counting easier.
 std::string Erase(std::string word)
 {
-    for(int i=0;i<word.size()-1;i++)    // a for loop for the word length
+    char *temp;
+    int counter=0;
+    for(int i=0;i<word.size();i++)    // a for loop for the word length
     {
         if(word[i]>='A' && word[i]<='Z')
             word[i] -= 32; // to lowercase
         // removing special characters
-        if(word[i] == '='  || word[i] == '"' || word[i] == '!' || word[i] == '@' || word[i] == '#' || word[i] == '$' || word[i] == '%' || word[i] == '^' || word[i] == '&' || word[i] == '*' || word[i] == '(' || word[i] == ')' || word[i] == '-' || word[i] == '_' || word[i] == '{' || word[i] == '}' || word[i] == '[' || word[i] == ']' || word[i] == '|' || word[i] == ';' || word[i] == ':' || word[i] == '<' || word[i] == '>' || word[i] == '?' || word[i] == '/' || word[i] == ',' || word[i] == '.' || word[i] == '\'' || word[i] == '~')
-            word[i] = ' ';
+        if(word[i] == '='  || word[i] == '"' || word[i] == '!' || word[i] == '@' || word[i] == '#' || word[i] == '$' || word[i] == '%' || word[i] == '^' || word[i] == '&' || word[i] == '*' || word[i] == '(' || word[i] == ')' || word[i] == '-' || word[i] == '_' || word[i] == '{' || word[i] == '}' || word[i] == '[' || word[i] == ']' || word[i] == '|' || word[i] == ';' || word[i] == ':' || word[i] == '<' || word[i] == '>' || word[i] == '?' || word[i] == '/' || word[i] == ',' || word[i] == '.' || word[i] == '\'' || word[i] == '~' || word[i] == '\t')
+        {
+            for(int j=i;j<word.size();j++)
+                word[j]=word[j+1];
+            counter++;
+        }
     }
-    return word;
+    temp = new char[word.size()-counter];
+    return temp;
 }
 
 
